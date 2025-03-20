69ef6dbca442804e5f7b86aaf481f03d993946dc
alan-w-u <alanwu0004@gmail.com>
2025-03-19 23:55:03 -0600

    add fileSystem

---
+++ src/fileSystem.ts
@@ -0,0 +1,38 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '../.env' })
+
+const dataDirectory = process.env.DATA_DIRECTORY as string
+
+export function write(
+  { fileName, fileContent, fileType = '.md', folderPath = dataDirectory }: { fileName: string, fileContent: any, fileType?: string, folderPath?: string }
+) {
+  // Ensure the target folder exists or create it if it does not
+  fs.mkdirSync(folderPath, { recursive: true })
+
+  const filePath = path.join(folderPath, fileName + fileType)
+
+  if (fileType === '.json') {
+    fs.writeFileSync(filePath, JSON.stringify(fileContent, null, 2), 'utf-8')
+  } else {
+    fs.writeFileSync(filePath, fileContent, 'utf-8')
+  }
+}
+
+export function erase(
+  { file, folderPath = dataDirectory }: { file: string, folderPath?: string }
+) {
+  fs.promises.unlink(path.join(folderPath, file))
+}
+
+export async function clear(
+  { folderPath = dataDirectory }: { folderPath?: string } = {}
+) {
+  const files = await fs.promises.readdir(folderPath)
+
+  for (const file of files) {
+    erase({ file, folderPath })
+  }
+}

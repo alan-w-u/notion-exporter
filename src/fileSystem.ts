b4e1c3192f66422ab4dcd0ab3ac0e01f193265b7
alan-w-u <alanwu0004@gmail.com>
2025-03-25 00:14:49 -0600

    update fileSystem functions

--- src/fileSystem.ts
+++ src/fileSystem.ts
@@ -9,14 +9,14 @@ dotenv.config({ path: '../.env' })
 const dataDirectory = process.env.DATA_DIRECTORY as string
 
 export function write(
-  { fileName, fileContent, fileExtension = '.md', folderPath = dataDirectory }: { fileName: string, fileContent: string, fileExtension?: string, folderPath?: string }
+  { fileName, fileContent, fileExtension = 'md', folderPath = dataDirectory }: { fileName: string, fileContent: string, fileExtension?: string, folderPath?: string }
 ): void {
   // Ensure the target folder exists or create it if it does not
   fs.mkdirSync(folderPath, { recursive: true })
 
-  const filePath = path.join(folderPath, fileName + fileExtension)
+  const filePath = path.join(folderPath, fileName + '.' + fileExtension)
 
-  if (fileExtension === '.json') {
+  if (fileExtension === 'json') {
     fs.writeFileSync(filePath, JSON.stringify(fileContent, null, 2), 'utf-8')
   } else {
     fs.writeFileSync(filePath, fileContent, 'utf-8')
@@ -36,5 +36,43 @@ export async function clear(
 
   for (const file of files) {
     erase({ file, folderPath })
+  }
+}
+
+export async function download(
+  { fileName, url }: { fileName: string, url: string }
+): Promise<string> {
+  try {
+    const assetsPath = path.join(dataDirectory, '/assets')
+
+    // Ensure the target folder exists or create it if it does not
+    fs.mkdirSync(assetsPath, { recursive: true })
+
+    // Make a GET request for URL content as a stream
+    const response = await axios.get(url, { responseType: 'stream' })
+
+    const contentType = response.headers['content-type']
+    const fileExtension = mime.extension(contentType)
+    const filePath = path.join(assetsPath, fileName + '.' + fileExtension)
+
+    // Return existing file path instead of downloading again if it already exists
+    if (fs.existsSync(filePath)) {
+      return filePath
+    }
+
+    // Create a write stream to save the file
+    const writer = fs.createWriteStream(filePath)
+    response.data.pipe(writer)
+
+    // Wait for the write stream to finish
+    await new Promise<void>((resolve, reject) => {
+      writer.on('finish', resolve)
+      writer.on('error', reject)
+    })
+
+    return filePath
+  } catch (error) {
+    console.error('Error downloading file:', error)
+    throw error
   }
 }

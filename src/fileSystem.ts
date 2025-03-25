ca25fd329f4af34985a59059a1b032d71e993fdb
alan-w-u <alanwu0004@gmail.com>
2025-03-24 23:53:54 -0600

    install mime-types

--- src/fileSystem.ts
+++ src/fileSystem.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import axios from 'axios'
+import mime from 'mime-types'
 import dotenv from 'dotenv'
 
 dotenv.config({ path: '../.env' })
@@ -35,34 +36,5 @@ export async function clear(
 
   for (const file of files) {
     erase({ file, folderPath })
-  }
-}
-
-export async function download(
-  { filename, url, fileExtension }: { filename: string, url: string, fileExtension: string }
-): Promise<void> {
-  try {
-    const assetsPath = path.join(dataDirectory, '/assets')
-
-    // Ensure the target folder exists or create it if it does not
-    fs.mkdirSync(assetsPath, { recursive: true })
-
-    const filePath = path.join(assetsPath, filename + fileExtension)
-
-    // Make a GET request for the stream response
-    const response = await axios.get(url, { responseType: 'stream' })
-
-    // Create a write stream to save the file
-    const writer = fs.createWriteStream(filePath)
-    response.data.pipe(writer)
-
-    // Wait for the write stream to finish
-    await new Promise<void>((resolve, reject) => {
-      writer.on('finish', resolve)
-      writer.on('error', reject)
-    })
-  } catch (error) {
-    console.error('Error downloading file:', error)
-    throw error
   }
 }

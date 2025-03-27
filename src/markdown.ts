84c7debf369796616e5886b2a6e1797a8b531f3d
alan-w-u <alanwu0004@gmail.com>
2025-03-26 20:40:14 -0600

    support all type conversions

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,3 +1,5 @@
+import * as util from './util'
+import * as fileSystem from './fileSystem'
 import {
   RichTextItemResponse,
   BlockObjectResponse,
@@ -36,10 +38,28 @@ import {
   UnsupportedBlockObjectResponse
 } from '@notionhq/client/build/src/api-endpoints'
 
-export const enumerationList: string[] = [
+export const indentTypes: string[] = [
   'bulleted_list_item',
-  'numbered_list_item'
+  'numbered_list_item',
+  'to_do',
+  'toggle'
 ]
+
+const captionTypes: string[] = [
+  'code',
+  'embed',
+  'bookmark',
+  'image',
+  'video',
+  'pdf',
+  'file'
+]
+
+const previewMap: { [key: string]: string } = {
+  'drive': 'https://s3-us-west-2.amazonaws.com/public.notion-static.com/8fb58690-ee50-4584-b9fd-ca9b524f56aa/google-drive-icon-19632.png',
+  'figma': 'https://www.notion.so/images/external_integrations/figma-icon.png',
+  'github': 'https://www.notion.so/images/external_integrations/github-icon.png'
+}
 
 const annotationMap: { [key: string]: (text: string) => string } = {
   'bold': bold,
@@ -49,62 +69,122 @@ const annotationMap: { [key: string]: (text: string) => string } = {
   'code': code
 }
 
-const typeMap: { [key: string]: (block: any) => string } = {
+const typeMap: { [key: string]: (block: any) => string | Promise<string> } = {
   'paragraph': paragraph,
   'heading_1': heading_1,
   'heading_2': heading_2,
   'heading_3': heading_3,
   'bulleted_list_item': bulleted_list_item,
   'numbered_list_item': numbered_list_item,
-  'callout': callout
+  'quote': quote,
+  'to_do': to_do,
+  'toggle': toggle,
+  'template': template,
+  'synced_block': synced_block,
+  'child_page': child_page,
+  'child_database': child_database,
+  'equation': equation,
+  'code': codeBlock,
+  'callout': callout,
+  'divider': divider,
+  'breadcrumb': breadcrumb,
+  'table_of_contents': table_of_contents,
+  'column_list': column_list,
+  'column': column,
+  'link_to_page': link_to_page,
+  'table': table,
+  'table_row': table_row,
+  'embed': embed,
+  'bookmark': bookmark,
+  'image': image,
+  'video': video,
+  'pdf': pdf,
+  'file': file,
+  'audio': audio,
+  'link_preview': link_preview,
+  'unsupported': unsupported
 }
 
-// Flag to determine whether to display block types with missing conversion functions
-const MISSING_TYPES = false
-
-export function convert(
-  block: BlockObjectResponse,
-  indentation: number
-): string {
+export async function convert(block: BlockObjectResponse, indentation: number): Promise<string> {
   let response = ''
   const type = block.type
 
   // Apply block type styling
   if (typeof typeMap[type] === 'function') {
-    response = typeMap[type](block)
-  } else if (MISSING_TYPES) {
-    response = `[${type.toUpperCase()}]`
+    response = await typeMap[type](block)
   }
 
-  // Add indentation
+  // Apply caption
+  if (captionTypes.includes(type)) {
+    response = response.concat('\n<br>', getCaption(block))
+  }
+
+  // Apply indentation
   if (indentation !== 0) {
     response = indent(response, indentation)
+  }
+
+  switch (type) {
+    case ('table'):
+      break
+    case ('table_row'):
+      response = response.concat('\n')
+      break
+    default:
+      response = response.concat('\n\n')
   }
 
   return response
 }
 
-function getText(
-  block: Partial<BlockObjectResponse>
-): string {
+function getText(block: Partial<BlockObjectResponse>): string {
   let response = ''
   const content = block[block.type as keyof BlockObjectResponse] as Object
 
-  if (content && 'rich_text' in content) {
-    for (const textContent of content.rich_text as RichTextItemResponse[]) {
-      if (textContent.type === 'text' && textContent.text) {
-        let text = textContent.text.content
-  
-        // Apply annotation styling
-        for (const [annotation, enabled] of Object.entries(textContent.annotations)) {
-          if (annotationMap[annotation] && enabled) {
-            text = annotationMap[annotation](text)
-          }
-        }
+  if ('rich_text' in content) {
+    response = formatContent(content.rich_text as RichTextItemResponse[])
+  }
 
-        response = response.concat(text)
+  return response
+}
+
+function getCaption(block: Partial<BlockObjectResponse>): string {
+  let response = ''
+  const content = block[block.type as keyof BlockObjectResponse] as Object
+
+  if ('caption' in content) {
+    response = formatContent(content.caption as RichTextItemResponse[])
+  }
+
+  return response
+}
+
+function formatContent(contentBlocks: RichTextItemResponse[]): string {
+  let response = ''
+
+  for (const contentBlock of contentBlocks) {
+    let content = ''
+
+    // Extract content
+    if (contentBlock.type === 'text') {
+      content = contentBlock.text.content
+    } else if (contentBlock.type === 'equation') {
+      content = `\$${contentBlock.equation.expression}\$`
+    }
+
+    // Apply annotation styling
+    for (const [annotation, enabled] of Object.entries(contentBlock.annotations)) {
+      if (annotationMap[annotation] && enabled) {
+        content = annotationMap[annotation](content)
       }
     }
+
+    // Apply link styling
+    if (contentBlock.type === 'text' && contentBlock.text.link) {
+      content = `[${content}](${contentBlock.text.link.url})`
+    }
+
+    response = response.concat(content)
   }
 
   return response
@@ -140,7 +220,7 @@ function code(text: string): string {
 function paragraph(block: ParagraphBlockObjectResponse): string {
   const text = getText(block)
 
-  return `${text}`
+  return text
 }
 
 function heading_1(block: Heading1BlockObjectResponse): string {
@@ -173,19 +253,247 @@ function numbered_list_item(block: NumberedListItemBlockObjectResponse): string 
   return `1. ${text}`
 }
 
-function callout(block: CalloutBlockObjectResponse): string {
+function quote(block: QuoteBlockObjectResponse): string {
+  const text = getText(block)
+
+  return `> ${text}`
+}
+
+function to_do(block: ToDoBlockObjectResponse): string {
+  const text = getText(block)
+
+  return `- [ ] ${text}`
+}
+
+function toggle(block: ToggleBlockObjectResponse): string {
+  const text = getText(block)
+
+  return `- ${text}`
+}
+
+function template(block: TemplateBlockObjectResponse): string {
+  // Deprecated
+  const text = getText(block)
+
+  return text
+}
+
+function synced_block(block: SyncedBlockBlockObjectResponse): string {
+  const text = getText(block)
+
+  return text
+}
+
+function child_page(block: ChildPageBlockObjectResponse): string {
+  const title = block.child_page.title
+  const urlTitle = title.replace(/\s+/g, '-')
+  const urlId = block.id.replace(/-/g, '')
+
+  return `[${title}](https://www.notion.so/${urlTitle}-${urlId})`
+}
+
+async function child_database(block: ChildDatabaseBlockObjectResponse): Promise<string> {
+  const title = block.child_database.title
+  const urlTitle = title.replace(/\s+/g, '%20')
+  const urlId = block.id.replace(/-/g, '')
+  let parentTitle = ''
+  let parentId = ''
+
+  switch (block.parent.type) {
+    case 'database_id':
+      parentId = block.parent.database_id
+      break
+    case 'page_id':
+      parentId = block.parent.page_id
+      break
+    case 'block_id':
+      parentId = block.parent.block_id
+      break
+  }
+
+  parentTitle = await util.getPageTitle(parentId)
+  parentTitle = parentTitle.replace(/\s+/g, '%20')
+  parentId = parentId.replace(/-/g, '')
+
+  return `[${title}](${parentTitle}%20${parentId}/${urlTitle}%20${urlId}.csv)`
+}
+
+function equation(block: EquationBlockObjectResponse): string {
+  const expression = block.equation.expression
+
+  return `$$\n${expression}\n$$`
+}
+
+function codeBlock(block: CodeBlockObjectResponse): string {
+  const code = getText(block)
+  const language = block.code.language
+
+  return `\`\`\`${language}\n${code}\n\`\`\``
+}
+
+async function callout(block: CalloutBlockObjectResponse): Promise<string> {
   const text = getText(block)
 
   switch (block.callout.icon?.type) {
     case 'emoji':
-      return `${block.callout.icon.emoji} ${text}`
+      return `<aside>\n${block.callout.icon.emoji} ${text}\n</aside>`
     case 'external':
-      return `<img src="${block.callout.icon.external.url}" width="25" style="vertical-align: middle"> ${text}`
+      const externalUrl = block.callout.icon.external.url
+      return `<aside>\n<img src="${externalUrl}" alt="${externalUrl}" width="25" style="vertical-align: middle" /> ${text}\n</aside>`
     case 'file':
-      return `${text}` // TODO:
+      const filePath = await fileSystem.download({ fileName: block.id, url: block.callout.icon.file.url })
+      return `<aside>\n<img src="${filePath}" alt="${filePath}" width="25" style="vertical-align: middle" /> ${text}\n</aside>`
     case 'custom_emoji':
-      return `${text}` // TODO:
+      const emojiUrl = block.callout.icon.custom_emoji.url
+      return `<aside>\n<img src="${emojiUrl}" alt="${emojiUrl}" width="25" style="vertical-align: middle" /> ${text}\n</aside>`
     default:
       return text
   }
+}
+
+function divider(block: DividerBlockObjectResponse): string {
+  return '---'
+}
+
+function breadcrumb(block: BreadcrumbBlockObjectResponse): string {
+  return ''
+}
+
+function table_of_contents(block: TableOfContentsBlockObjectResponse): string {
+  return ''
+}
+
+function column_list(block: ColumnListBlockObjectResponse): string {
+  return ''
+}
+
+function column(block: ColumnBlockObjectResponse): string {
+  return ''
+}
+
+async function link_to_page(block: LinkToPageBlockObjectResponse): Promise<string> {
+  let urlId = ''
+  let title = ''
+
+  switch (block.link_to_page.type) {
+    case 'database_id':
+      urlId = block.link_to_page.database_id
+      title = await util.getDatabaseTitle(urlId)
+      break
+    case 'page_id':
+      urlId = block.link_to_page.page_id
+      title = await util.getPageTitle(urlId)
+      break
+    case 'comment_id':
+      urlId = block.link_to_page.comment_id
+      break
+  }
+
+  urlId = urlId.replace(/-/g, '')
+
+  return `[${title}](https://www.notion.so/${urlId})`
+}
+
+function table(block: TableBlockObjectResponse): string {
+  return ''
+}
+
+function table_row(block: TableRowBlockObjectResponse): string {
+  const cells = block.table_row.cells
+  let row = ''
+
+  for (const cell of cells) {
+    row = row.concat('| ', formatContent(cell), ' ')
+  }
+
+  row = row.concat('|\n', '| --- '.repeat(cells.length), '|')
+
+  return row
+}
+
+function embed(block: EmbedBlockObjectResponse): string {
+  const url = block.embed.url
+  const fileName = url.split('/').pop()
+
+  return `[${fileName}](${url})`
+}
+
+function bookmark(block: BookmarkBlockObjectResponse): string {
+  const url = block.bookmark.url
+  const domain = new URL(url).hostname
+
+  return `[${domain}](${url})`
+}
+
+async function image(block: ImageBlockObjectResponse): Promise<string> {
+  switch (block.image.type) {
+    case ('external'):
+      return `![](${block.image.external.url})`
+    case ('file'):
+      const filePath = await fileSystem.download({ fileName: block.id, url: block.image.file.url })
+      return `![](${filePath})`
+  }
+}
+
+async function video(block: VideoBlockObjectResponse): Promise<string> {
+  switch (block.video.type) {
+    case ('external'):
+      const url = block.video.external.url
+      return `[${url}](${url})`
+    case ('file'):
+      const filePath = await fileSystem.download({ fileName: block.id, url: block.video.file.url })
+      const fileName = filePath.split('/').pop()
+      return `[${fileName}](${filePath})`
+  }
+}
+
+async function pdf(block: PdfBlockObjectResponse): Promise<string> {
+  switch (block.pdf.type) {
+    case ('external'):
+      const url = block.pdf.external.url
+      const title = url.split('/').pop() || ''
+      return `[${title}](${url})`
+    case ('file'):
+      const filePath = await fileSystem.download({ fileName: block.id, url: block.pdf.file.url })
+      const fileName = filePath.split('/').pop() || ''
+      return `[${fileName}](${filePath})`
+  }
+}
+
+async function file(block: FileBlockObjectResponse): Promise<string> {
+  const fileName = block.file.name
+
+  switch (block.file.type) {
+    case ('external'):
+      return `[${fileName}](${block.file.external.url})`
+    case ('file'):
+      const filePath = await fileSystem.download({ fileName: block.id, url: block.file.file.url })
+      return `[${fileName}](${filePath})`
+  }
+}
+
+async function audio(block: AudioBlockObjectResponse): Promise<string> {
+  switch (block.audio.type) {
+    case ('external'):
+      const url = block.audio.external.url
+      return `[${url}](${url})`
+    case ('file'):
+      const filePath = await fileSystem.download({ fileName: block.id, url: block.audio.file.url })
+      const fileName = filePath.split('/').pop()
+      return `[${fileName}](${filePath})`
+  }
+}
+
+function link_preview(block: LinkPreviewBlockObjectResponse): string {
+  const url = block.link_preview.url
+  const title = url.split('/').pop()
+  const domain = new URL(url).hostname
+  const sld = domain.split('.')[0]
+  const previewUrl = previewMap[sld]
+
+  return `[<img src="${previewUrl}" alt="${previewUrl}" width="25" style="vertical-align: middle" /> ${title}](${url})`
+}
+
+function unsupported(block: UnsupportedBlockObjectResponse): string {
+  return ''
 }
--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,7 @@ import * as notion from './notion'
 import * as markdown from './markdown'
 import {
   QueryDatabaseParameters,
+  DatabaseObjectResponse,
   PageObjectResponse,
   BlockObjectResponse
 } from '@notionhq/client/build/src/api-endpoints'
@@ -25,6 +26,14 @@ export async function getPageIds(
   return response.results.map(page => page.id) || []
 }
 
+export async function getDatabaseTitle(
+  databaseId: string
+): Promise<string> {
+  const database = await notion.getDatabase({ databaseId }) as DatabaseObjectResponse
+
+  return database.title[0].plain_text
+}
+
 export async function getPageTitle(
   pageId: string
 ): Promise<string> {
@@ -44,23 +53,29 @@ export async function parsePage(
   indentation: number = 0
 ): Promise<string> {
   const block = await notion.getBlock({ blockId }) as BlockObjectResponse
+  const type = block.type
+
+  // Remove the blank row at the end of a table
+  if (type !== 'table_row' && content.value.trim().endsWith('| --- |')) {
+    content.value = content.value.slice(0, content.value.lastIndexOf('\n|')).concat('\n\n')
+  }
 
   // Save content to the accumulator
-  content.value = content.value.concat(markdown.convert(block, indentation), '\n')
+  content.value = content.value.concat(await markdown.convert(block, indentation))
 
   // Base case
   if (!block.has_children) {
     return ''
   }
 
-  // Indentation for nested lists
-  if (markdown.enumerationList.includes(block.type)) {
+  // Indentation for nested items
+  if (markdown.indentTypes.includes(type)) {
     indentation++
   }
 
   const blocks = await notion.getBlockChildren({ blockId })
 
-  // Traverse children blocks
+  // Traverse child blocks
   for (const block of blocks.results as BlockObjectResponse[]) {
     await parsePage(block.id, content, indentation)
   }

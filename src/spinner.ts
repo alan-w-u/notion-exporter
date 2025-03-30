e097b32ba14658373324d17562c7da6dc921662b
alan-w-u <alanwu0004@gmail.com>
2025-03-30 13:08:34 -0600

    refactor spinner

--- src/spinner.ts
+++ src/spinner.ts
@@ -1,37 +1,54 @@
 import * as notion from './notion'
-const spinnerFrames = ['⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏', '⠋']
-const spinnerUpdateSpeed = 100 // milliseconds
+import * as util from './util'
+
+const SPINNER_FRAMES = ['⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏', '⠋']
+const SPINNER_UPDATE_SPEED = 100 // milliseconds
+const DATE_TIME_FORMAT_OPTIONS: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: true
+}
 
 let spinnerInterval: NodeJS.Timeout
-let spinnerFrame = 0
-let duration = 0
+let spinnerFrame: number = 0
+let startTime: Date
 
 export function start() {
-  process.stdout.write('\x1b[?25l') // Hide cursor
+  startTime = new Date()
+
+  // Hide cursor
+  process.stdout.write('\x1b[?25l')
 
   spinnerInterval = setInterval(() => {
-    process.stdout.write(`\r${spinnerFrames[spinnerFrame]}`)
-    spinnerFrame = (spinnerFrame + 1) % spinnerFrames.length
-    duration++
-  }, spinnerUpdateSpeed)
+    process.stdout.write(`\r${SPINNER_FRAMES[spinnerFrame]}`)
+    spinnerFrame = (spinnerFrame + 1) % SPINNER_FRAMES.length
+  }, SPINNER_UPDATE_SPEED)
 }
 
 export function stop() {
-  const seconds = duration / (1000 / spinnerUpdateSpeed)
-  const minutes = Math.floor(seconds / 60)
-  const remainingSeconds = Math.round(seconds % 60)
+  const stopTime = new Date()
+  const duration = (stopTime.getTime() - startTime.getTime()) / 1000
+  const minutes = Math.floor(duration / 60)
+  const seconds = Math.round(duration % 60)
 
-  process.stdout.write('\r\x1b[K') // Clear remaining spinner frame
-  process.stdout.write('\x1b[?25h') // Show cursor
-  process.stdout.write('\x1b[1m\x1b[32m✔\x1b[0m Script run time: ')
+  // Clear remaining spinner frame
+  process.stdout.write('\r\x1b[K')
+  // Show cursor
+  process.stdout.write('\x1b[?25h')
 
-  if (minutes > 0) {
-    console.log(`\x1b[1m\x1b[32m${minutes}m ${remainingSeconds}s\x1b[0m`)
-  } else {
-    console.log(`\x1b[1m\x1b[32m${seconds}s\x1b[0m`)
-  }
-
+  // Print metrics
+  console.log(`\x1b[1m\x1b[32m✔\x1b[0m Completion time: \x1b[1m\x1b[34m${stopTime.toLocaleString('en-US', DATE_TIME_FORMAT_OPTIONS)}\x1b[0m`)
+  console.log(`\x1b[1m\x1b[32m✔\x1b[0m Script run time: \x1b[1m\x1b[32m${minutes}m ${seconds}s\x1b[0m`)
   console.log(`\x1b[1m\x1b[32m✔\x1b[0m Notion requests: \x1b[1m\x1b[33m${notion.requests}\x1b[0m`)
+
+  // Print error messages
+  for (const error of util.errors) {
+    console.log(error)
+  }
 
   clearInterval(spinnerInterval)
 }

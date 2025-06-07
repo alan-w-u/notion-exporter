00b58fb7269e731c24e6d30c8b8a99fcd72cd1c5
alan-w-u <alanwu0004@gmail.com>
2025-06-06 18:28:48 -0600

    implement round-robin scheduling

--- src/notion.ts
+++ src/notion.ts
@@ -11,114 +11,85 @@ import {
 
 dotenv.config({ path: '../.env' })
 
-const notionInstances: { client: Client, free: boolean, requests: number }[] = []
+export let requests = 0
 
-function initialize(): void {
-  // Populate Notion API keys
-  const notionKeys = Object.keys(process.env)
-    .filter(key => key.startsWith('NOTION_API_KEY'))
-    .map(key => process.env[key])
-    .filter(Boolean) as string[]
+let roundRobinIndex = 0
 
-  // Populate Notion clients
-  notionInstances.push(...notionKeys.map(notionKey => ({
-    client: new Client({ auth: notionKey }),
-    free: true,
-    requests: 0
-  })))
-}
+const notionKeys = Object.keys(process.env)
+  .filter(key => key.startsWith('NOTION_API_KEY'))
+  .map(key => process.env[key])
+  .filter(Boolean) as string[]
 
-initialize()
+const notionClients: Client[] = notionKeys.map(notionKey => new Client({ auth: notionKey }))
 
-function notionInstance(): any {
-  return notionInstances.find(notionInstance => notionInstance.free)
-}
+function notion(): Client {
+  const notion = notionClients[roundRobinIndex]
+  roundRobinIndex = (roundRobinIndex + 1) % notionClients.length
 
-export function requests(): number {
-  return notionInstances.reduce((requests, notionInstance) => requests + notionInstance.requests, 0)
+  return notion
 }
 
 export async function queryDatabase(
   { databaseId, opts = {} }: { databaseId: string, opts?: Partial<QueryDatabaseParameters> }
 ): Promise<QueryDatabaseResponse> {
-  const notion = notionInstance()
-  notion.free = false
-
   try {
-    return await notion.client.databases.query({ database_id: databaseId, ...opts })
+    return await notion().databases.query({ database_id: databaseId, ...opts })
   } catch (error) {
     console.error('Error querying database:', error)
     throw error
   } finally {
-    notion.free = true
-    notion.requests++
+    requests++
   }
 }
 
 export async function getDatabase(
   { databaseId }: { databaseId: string }
 ): Promise<GetDatabaseResponse> {
-  const notion = notionInstance()
-  notion.free = false
-
   try {
-    return await notion.client.databases.retrieve({ database_id: databaseId })
+    return await notion().databases.retrieve({ database_id: databaseId })
   } catch (error) {
     console.error('Error retrieving database:', error)
     throw error
   } finally {
-    notion.free = true
-    notion.requests++
+    requests++
   }
 }
 
 export async function getPage(
   { pageId }: { pageId: string }
 ): Promise<GetPageResponse> {
-  const notion = notionInstance()
-  notion.free = false
-
   try {
-    return await notion.client.pages.retrieve({ page_id: pageId })
+    return await notion().pages.retrieve({ page_id: pageId })
   } catch (error) {
     console.error('Error retrieving page:', error)
     throw error
   } finally {
-    notion.free = true
-    notion.requests++
+    requests++
   }
 }
 
 export async function getBlock(
   { blockId }: { blockId: string }
 ): Promise<GetBlockResponse> {
-  const notion = notionInstance()
-  notion.free = false
-
   try {
-    return await notion.client.blocks.retrieve({ block_id: blockId })
+    return await notion().blocks.retrieve({ block_id: blockId })
   } catch (error) {
     console.error('Error retrieving block children:', error)
     throw error
   } finally {
-    notion.free = true
-    notion.requests++
+    requests++
   }
 }
 
 export async function getBlockChildren(
   { blockId }: { blockId: string }
 ): Promise<ListBlockChildrenResponse> {
-  const notion = notionInstance()
-  notion.free = false
-
   try {
-    return await notion.client.blocks.children.list({ block_id: blockId })
+    return await notion().blocks.children.list({ block_id: blockId })
   } catch (error) {
     console.error('Error retrieving block children:', error)
     throw error
   } finally {
-    notion.free = true
-    notion.requests++
+    requests++
   }
 }
